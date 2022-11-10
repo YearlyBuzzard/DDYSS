b3d7269541750d9de5f4bc50774439c54de744ac
YearlyBuzzard <75999037+YearlyBuzzard@users.noreply.github.com>
2022-11-09 23:03:57 -0600

    Update ProfileUser.java

--- app/src/main/java/com/example/ddyssadmin/ProfileUser.java
+++ app/src/main/java/com/example/ddyssadmin/ProfileUser.java
@@ -2,26 +2,30 @@ package com.example.ddyssadmin;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileUser extends Fragment {
 
  private FirebaseAuth mAuth;
  private Button btnLogout;
 
- private TextView txtname;
- private TextView txtemail;
- private TextView txtnumber;
+ private TextView mFullName, mEmail, mPassword, mPhone;
+ private DatabaseReference mDatabase;
 
 
  @Override
@@ -30,12 +34,11 @@ public class ProfileUser extends Fragment {
   View root = inflater.inflate(R.layout.fragment_profile_user, container, false);
 
   mAuth = FirebaseAuth.getInstance();
-
+  mDatabase = FirebaseDatabase.getInstance().getReference();
   btnLogout = (Button)root.findViewById(R.id.btnLogout);
-  txtname = (TextView)root.findViewById(R.id.txtname);
-  txtemail = (TextView)root.findViewById(R.id.txtemail);
-  txtnumber = (TextView)root.findViewById(R.id.txtnumber);
-
+  mFullName = (TextView)root.findViewById(R.id.txtname);
+  mEmail = (TextView)root.findViewById(R.id.txtemail);
+  mPhone = (TextView)root.findViewById(R.id.txtnumber);
 
   btnLogout.setOnClickListener(new View.OnClickListener() {
 
@@ -48,8 +51,32 @@ public class ProfileUser extends Fragment {
    }
   });
 
+  getUserInfo();
+  return root;
 
-  return  root;
  }
 
+  private void getUserInfo() {
+  String id = mAuth.getCurrentUser().getUid();
+   mDatabase.child("User").child(id).addValueEventListener(new ValueEventListener(){
+
+    @Override
+    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+      if (dataSnapshot.exists()) {
+       String Name = dataSnapshot.child("Name").getValue().toString();
+       String Email = dataSnapshot.child("Email").getValue().toString();
+       String Phone = dataSnapshot.child("Phone").getValue().toString();
+
+       mFullName.setText(Name);
+       mEmail.setText(Email);
+       mPhone.setText(Phone);
+      }
+    }
+
+    @Override
+    public void onCancelled(@NonNull DatabaseError error) {
+
+    }
+   });
+}
 }
